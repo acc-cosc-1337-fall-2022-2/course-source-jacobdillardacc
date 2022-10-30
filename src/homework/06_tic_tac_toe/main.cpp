1e0b3ddd0fa3881ec448a002521ec90a2f2f94e9
jacobdillardacc <jacob.dillard@g.austincc.edu>
2022-10-29 23:57:31 -0500

    hw 6 w test cases

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,38 @@
+#import "tic_tac_toe.h"
 
-int main() 
-{
+int main() {
+    TicTacToe game;
 
-	return 0;
+    string first_player;
+    int end = 0;
+    char player_choice;
+    int position;
+
+    std::cout << "1|2|3\n"
+                 "4|5|6\n"
+                 "7|8|9\n";
+
+    do {
+        std::cout << "Player 1, Choose X or O:\n";
+        std::cin >> first_player;
+
+        game.start_game(first_player);
+
+        while (!game.game_over()) {
+            game.display_board();
+            std::cout << "Player " << game.get_player() << ", select a position 1-9: \n";
+            std::cin >> position;
+            game.mark_board(position);
+        }
+
+        game.display_board();
+        std::cout << "\nWould you like to exit? (Y/N): ";
+        std::cin >> player_choice;
+        std::cout << "\n";
+        if (player_choice == 'Y' || player_choice == 'y') {
+            end = 1;
+        }
+    } while (end != 1);
+
+    return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,38 @@
 //h
+#include <iostream>
+#include <string>
+#include<vector>
+
+#ifndef TicTacToe_H
+#define TicTacToe_H
+
+using
+        std::string,
+        std::vector;
+
+class TicTacToe {
+    vector<string> pegs{" ", " ", " ",
+                        " ", " ", " ",
+                        " ", " ", " "};
+    string player;
+
+public:
+    bool game_over();
+
+    void start_game(string first_player);
+
+    void mark_board(int position);
+
+    string get_player() const;
+
+    void display_board() const;
+
+private:
+    void set_next_player();
+
+    bool check_board_full();
+
+    void clear_board();
+};
+
+#endif
