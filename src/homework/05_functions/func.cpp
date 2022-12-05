4c2b141ef9908b9200220e5e1a0977df910d61a6
jacobdillardacc <jacob.dillard@g.austincc.edu>
2022-12-04 20:51:08 -0600

    hw 5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,34 @@
 //add include statements
+#include "func.h"
 
 //add function code here
+
+double get_gc_content(const string &dna) {
+    double gc_content = 0;
+    for (int i = 0; i < dna.length(); i ++) {
+        if (dna[i] == 'C' || dna[i] == 'G') {
+            gc_content++;
+        }
+    }
+    return gc_content / dna.length();
+}
+
+string get_dna_complement(string dna) {
+    string complement;
+    for (int i = 0; i < dna.length(); i++) {
+        if (dna[i] == 'A') { complement += 'T'; }
+        else if (dna[i] == 'T') { complement += 'A'; }
+        else if (dna[i] == 'C') { complement += 'G'; }
+        else if (dna[i] == 'G') { complement += 'C'; }
+    }
+    complement = get_reverse_string(complement);
+    return complement;
+}
+
+string get_reverse_string(string dna) {
+    string reverse;
+    for (int i = dna.length() - 1; i >= 0; i--) {
+        reverse += dna[i];
+    }
+    return reverse;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,14 +1,47 @@
-/*
-use a vector of int with values 8, 4, 20, 88, 66, 99
+#include "func.h"
 
-Prompt user for 1 for Get Max from vector and 2 for Get primes.
-Prompt user for a number and return max value or list of primes
-and display them to screen.
-Program continues until user decides to exit.
+int main() {
 
-*/
-int main() 
-{
+    char quit = 'N';
 
-	return 0;
+    while (quit == 'N' || quit == 'n') {
+        int selection;
+        cout << "\n          MENU\n"
+                "1 - Get GC Content\n"
+                "2 - Get DNA Complement\n"
+                "3 - Exit\n\n"
+                "Menu Selection: ";
+        cin >> selection;
+
+        if (selection == 1) {
+            string dna;
+            cout << "\nEnter DNA string: ";
+            cin >> dna;
+
+            for (char &i: dna) {
+                i = toupper(i);
+            }
+
+            cout << "\n" << get_gc_content(dna) << "\nExit? (Y/N): ";
+            cin >> quit;
+        }
+
+        else if (selection == 2) {
+            string dna;
+            cout << "\nEnter DNA string: ";
+            cin >> dna;
+
+            for (char &i: dna) {
+                i = toupper(i);
+            }
+
+            cout << "\n" << get_dna_complement(dna) << "\nExit? (Y/N): ";
+            cin >> quit;
+        }
+
+        else if (selection == 3) {
+            exit(0);
+        }
+    }
+    return 0;
 }
